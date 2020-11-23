2a5c131a1fa7552bfd976ba579ea61e9f40e67e0
Freddy <freddy.vaca@iris-worldwide.com>
2020-11-23 00:02:08 +0000

    Solution and projects creation

---
+++ HMSD-CodingTest.APIGateway.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace HMSD_CodingTest.APIGateway.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ HMSD-CodingTest.EncryptionService.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace HMSD_CodingTest.EncryptionService.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
