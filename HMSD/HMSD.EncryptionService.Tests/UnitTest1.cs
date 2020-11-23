da48046ca0c0881934aee3104f05446a6df540cc
Freddy <freddy.vaca@iris-worldwide.com>
2020-11-23 00:25:16 +0000

    Creating solution and adding EncryptionService projects

---
+++ HMSD/HMSD.EncryptionService.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace HMSD.EncryptionService.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
